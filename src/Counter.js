b6bf6b0b3b0bb5eb21dbec6069f0b3f65966f299
chitragoenka <chitra.goenka@gmail.com>
2023-04-26 16:06:32 -0400

    Added styling

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,6 @@
 // import the necessary dependencies
 import React, { useState } from 'react';
-
+import "./counter.css";
 
 function Counter() {
     const [count, setCount] = useState(0);
@@ -18,11 +18,11 @@ function Counter() {
     }
 
     return (
-        <div>
+        <div className='section'>
             <h1>Count: {count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={reset}>Reset</button>
+            <button className='increment' onClick={increment}>Increment</button>
+            <button className='decrement' onClick={decrement}>Decrement</button>
+            <button className='reset' onClick={reset}>Reset</button>
         </div>
     );
 } 
